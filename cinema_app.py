9423222c040fb14f8900a6c24008047cf0f47e1a
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2022-10-22 11:18:31 +0700

    Combination of all frames, namely the app itself

---
+++ cinema_app.py
@@ -0,0 +1,14 @@
+# Program: Aplikasi Cinema
+# 1. Dewantoro Triatmojo / 19622152
+# 2. Randy Verdian / 19622202
+# 3. Aira Ardistya Akbarsyah/ 16522062
+# 4. Berto Togatorop/ 19622192
+
+# Algoritma:
+
+import tkinter as tk
+from tkinter import ttk
+import locale
+import datetime
+from database import list_movie
+
