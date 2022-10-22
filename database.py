18ba3a091440dde0033be303391f592d93e1eb25
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2022-10-22 08:36:11 +0700

    Database cinema-app

---
+++ database.py
@@ -0,0 +1,245 @@
+list_user = [
+    {
+        "nama": ...,
+        "email": ...,
+        "password": ...,
+        "saldo": ...,
+    },
+    {
+        "nama": ...,
+        "email": ...,
+        "password": ...,
+        "saldo": ...,
+    },
+]
+
+list_movie = [
+    {
+        "file": "C:/Users/Dewo/Desktop/22BADM.png",
+        "title": "Black Adam",
+        "age": "R13",
+        "genre": "Action, Fantasy, Sci-Fi",
+        "duration": "125 Minutes",
+        "plot": "Berkisah tentang sosok antihero yang mendapatkan kekuatan dari dewa mesir bernama Adam. Ia datang untuk menciptakan keadilan di dunia saat ini.",
+        "producer": "Beau Flynn, Dany Garcia, Hiram Garcia",
+        "director": "Jaume Collet-serra",
+        "writer": "Adam Sztykiel, Rory Haines, Sohrab Noshirvani",
+        "Cast:": "Dwayne Johnson, Viola Davis, Sarah Shahi, Pierce Brosnan, Noah Centineo, Aldis Hodge, Angel Rosario Jr., Joseph Gatt, Mohammed Amer, Quintessa Swindell",
+        "price": 45000,
+        "sold_seat": {
+            "bsd": {
+                "today": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                    },
+                "tomorrow": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                },
+            },
+            "bandung": {
+                "today": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                    },
+                "tomorrow": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                },
+            },
+            "jakarta": {
+                "today": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                    },
+                "tomorrow": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                },
+            },
+        },
+    },
+    {
+        "file": "C:/Users/Dewo/Desktop/22AMSM.png",
+        "title": "AMSTERDAM",
+        "age": "R13",
+        "genre": "Drama, History",
+        "duration": "134 Minutes",
+        "plot": "Berlatar tahun 1930-an, tiga sahabat Burt Berendsen (Christian Bale), Valerie Voze (Margot Robbie) dan Harold Woodman (John David Washington) adalah saksi pembunuhan yang akhirnya menjadi tersangka. Ketiganya berusaha mencari jalan keluar dan mengubah jalan sejarah Amerika.",
+        "producer": "Christian Bale, Matthew Budman, Anthony Katagas, Arnon Milchan, David O. Russell",
+        "director": "David O. Russell",
+        "writer": "David O. Russell",
+        "Cast:": "Christian Bale, Margot Robbie, John David Washington, Rami Malek, Mike Myers, Taylor Swift, Zoe Saldana, Robert De Niro, Anya Taylor-joy, Chris Rock, Michael Shannon.",
+        "price": 40000,
+        "sold_seat": {
+            "bsd": {
+                "today": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                    },
+                "tomorrow": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                },
+            },
+            "bandung": {
+                "today": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                    },
+                "tomorrow": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                },
+            },
+            "jakarta": {
+                "today": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                    },
+                "tomorrow": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                },
+            },
+        },
+    },
+    {
+        "file": "C:/Users/Dewo/Desktop/22DOCG.png",
+        "title": "DOCTOR G",
+        "age": "R17+",
+        "genre": "Comedy",
+        "duration": "122 Minutes",
+        "plot": "Sebuah drama komedi kampus kedokteran, Doctor G adalah tentang perjuangan yang lucu Dr. Uday Gupta, dia mempunyai keinginan untuk berspesialisasi dalam Ortopedi, tetapi terjebak di kelas Ginekologi yang semuanya perempuan.",
+        "producer": "Vineet Jain",
+        "director": "Anubhuti Kashyap",
+        "writer": "Saurabh Bharat",
+        "Cast:": "Ayushmann Khuranna, Rakul Preet Singh, Shefali Shah, Sheeba Chaddha, Abhinay Raj Singh, Paresh Pahuja, Jhumma Mitra, Azzy Bagria",
+        "price": 40000,
+        "sold_seat": {
+            "bsd": {
+                "today": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                    },
+                "tomorrow": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                },
+            },
+            "bandung": {
+                "today": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                    },
+                "tomorrow": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                },
+            },
+            "jakarta": {
+                "today": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                    },
+                "tomorrow": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                },
+            },
+        },
+    },
+    {
+        "file": "C:/Users/Dewo/Desktop/22OPFR.png",
+        "title": "ONE PIECE FILM: RED",
+        "age": "R13",
+        "genre": "Animation, Adventure, Fantasy",
+        "duration": "115 Minutes",
+        "plot": "Untuk pertama kalinya, Uta - penyanyi paling dicintai akan mengungkapkan dirinya kepada dunia di konser langsungnya. Suara yang ditunggu-tunggu oleh seluruh dunia akan segera bergema.",
+        "producer": "Eiichiro Oda",
+        "director": "Goro Taniguchi",
+        "writer": "Tsutomu Kuroiwa",
+        "Cast:": "Kazuya Nakai, Kaori Nazuka, Akemi Okamura, Kappei Yamaguchi, Mayumi Tanaka, Yuriko Yamaguchi, Hiroaki Hirata, Shuichi Ikeda",
+        "price": 50000,
+        "sold_seat": {
+            "bsd": {
+                "today": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                    },
+                "tomorrow": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                },
+            },
+            "bandung": {
+                "today": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                    },
+                "tomorrow": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                },
+            },
+            "jakarta": {
+                "today": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                    },
+                "tomorrow": {
+                    "time1": [[False for j in range(15)] for i in range(9)],
+                    "time2": [[False for j in range(15)] for i in range(9)],
+                    "time3": [[False for j in range(15)] for i in range(9)],
+                    "time4": [[False for j in range(15)] for i in range(9)],
+                },
+            },
+        },
+    },
+]
