6726b8e3ca5ac9701f7fa7a781d7abdcbe6b6132
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2022-10-29 22:03:46 +0700

    Updated Header style and added logout button

--- header.py
+++ header.py
@@ -1,28 +1,27 @@
 import tkinter as tk
-from tkinter import ttk
-from PIL import Image, ImageTk
 
 window = tk.Tk()
 window.title("Cinema XX5")
-screen_width = window.winfo_screenwidth()
-screen_height = window.winfo_screenheight()
-window.geometry(f"{screen_width}x{screen_height}")
 
-header_frame = tk.Frame(window, highlightbackground="orange", highlightthickness=3)
+img_xx5_heading = tk.PhotoImage(file="images/xx5heading.png")
 
-image = Image.open("images/xx5.png")
-resize_image = image.resize((180, 100))
-img = ImageTk.PhotoImage(resize_image)
-left_frame = tk.Frame(header_frame)
-label_gambar = tk.Label(left_frame, image=img).pack(side="left")
-now_playing = tk.Button(left_frame, text="Now Playing", font=("arial", 16)).pack(side="left", anchor="center")
-up_coming = tk.Button(left_frame, text="Up Coming", font=("arial", 16)).pack(side="right",anchor="center")
-left_frame.pack(side="left")
-right_frame = tk.Frame(header_frame)
-saldo = tk.Button(right_frame, text="Rp100.000,00", font=("arial", 16)).pack(side="left")
-akun = tk.Label(right_frame, text="Erling Haaland", font=("arial", 16)).pack(side="right")
-right_frame.pack(side="right", anchor="center")
+def HeaderFrame():
+    header_frame = tk.Frame(window, width=1200, bg="#171a30")
+    header_frame.pack(fill="x")
+    # Left Frame (Logo, Now Playing, Upcoming)
+    left_frame = tk.Frame(header_frame, bg="#171a30")
+    left_frame.pack(side="left")
+    label_gambar = tk.Label(left_frame, image=img_xx5_heading, bg="#171a30").pack(side="left", padx=10)
+    now_playing = tk.Button(left_frame, text="Now Playing", font=("arial", 16)).pack(side="left", anchor="center", padx=10)
+    up_coming = tk.Button(left_frame, text="Up Coming", font=("arial", 16)).pack(side="right",anchor="center", padx=10)
 
-header_frame.pack(fill="x")
+    # Right Frame (Saldo, Loglout, Name)
+    right_frame = tk.Frame(header_frame, bg="#171a30")
+    right_frame.pack(side="right")
+    saldo = tk.Button(right_frame, text="Rp100.000,00", font=("arial", 16)).pack(side="left", padx=10)
+    logout = tk.Button(right_frame, text="Log Out", font=("arial", 16)).pack(side="left", padx=10)
+    akun = tk.Label(right_frame, text="Erling Haaland", font=("arial", 16)).pack(side="right", padx=10)
+
+HeaderFrame()
 
 window.mainloop()
